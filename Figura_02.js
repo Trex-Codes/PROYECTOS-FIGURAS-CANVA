89341a88d0a79aa2853f0db8ef33765446a6815f
Trex_Codes <juan.13JSG@gmail.com>
2020-06-18 22:56:19 -0500

    11.0 Lines and continue Working Figure 2

--- Figura_02.js
+++ Figura_02.js
@@ -16,27 +16,16 @@ ctx.stroke();
 ctx.fill();
 ctx.closePath();
 
-// linea derecha central - CUBO 
+// LINEA CUBO IZQUIERDA 
 ctx.beginPath();
-ctx.moveTo(150, 145); // Linea derecha (BASE CUBO)
-ctx.lineTo(220, 100);
-ctx.lineTo(220, 55); // linea 90° derecha 
-// ctx.lineTo(150, 100); // conectores linea central superior
-ctx.stroke();
-ctx.closePath();
-
-// linea izquierda central - CUBO
-ctx.beginPath();
-ctx.moveTo(150, 145); // Liena izquierda (BASE CUBO)
-ctx.lineTo(50, 105);
-ctx.lineTo(50, 60); // linea 90° izquierda
-// ctx.lineTo(150, 100); // conectores linea central superior
+ctx.moveTo(35, 95);
+ctx.lineTo(65, 105);
 ctx.stroke();
 ctx.closePath();	
 
 // linea atras derecha - (BASE CUBO)
 ctx.beginPath();
-ctx.moveTo(220, 55); // Linea atras Union Cubo (SUPERIOR)
+ctx.moveTo(147, 30); // Linea atras Union Cubo (SUPERIOR)
 ctx.lineTo(120, 20); 
 ctx.stroke();
 ctx.closePath();
@@ -50,18 +39,28 @@ ctx.closePath();
 
 // cuadro izquierdo completo
 ctx.beginPath();
-ctx.lineTo(120, 20);
-ctx.lineTo(50, 60); // Conector base Arriba - Base Abajo 
+ctx.moveTo(120, 20);
+ctx.lineTo(95, 35); // Conector base Arriba - Base Abajo 
 ctx.stroke();
 ctx.closePath();
 
-//  LINEAS INFERIORES (LATERAL - FRONTAL)
-ctx.beginPath();
-ctx.moveTo(50, 90); // Linea barra Base 2.0 Izquierda 
-ctx.lineTo(150, 130);
-ctx.lineTo(220, 85); // Linea barra base 2.0 (Vista  frontal- lateral)
-ctx.stroke();
-ctx.closePath();
+	// LINEA INFERIOR (1)
+	ctx.beginPath();
+	ctx.moveTo(120, 120);
+	ctx.lineTo(150, 130);
+	ctx.lineTo(195, 100);
+	ctx.stroke();
+	ctx.closePath();
+
+	// LINEA INFERIOR (2)
+	ctx.beginPath();
+	ctx.moveTo(120, 135);
+	ctx.lineTo(150, 145);
+	ctx.moveTo(150, 145); // Linea derecha (BASE CUBO)
+	ctx.lineTo(220, 100);
+	ctx.lineTo(220, 55); // linea 90° derecha 
+	ctx.stroke();
+	ctx.closePath();
 
 // --------------------------------------------
 
@@ -71,7 +70,10 @@ ctx.moveTo(195, 115);
 ctx.lineTo(195, 70);
 ctx.lineTo(95, 35);
 ctx.lineTo(95, 65);
-ctx.lineTo(50, 90);
+ctx.lineTo(35, 95);
+ctx.lineTo(35, 110);
+ctx.lineTo(35, 110);
+ctx.lineTo(65, 120);
 ctx.stroke();
 ctx.closePath();
 				
@@ -91,25 +93,65 @@ ctx.lineTo(95, 65);
 ctx.stroke();
 ctx.closePath();
 
-// LINEA MEDIO BASE INFERIOR (2)
-ctx.beginPath();
-ctx.moveTo(180, 110);
-ctx.lineTo(79, 75);
-ctx.stroke();
-ctx.closePath();
+
+			// Linea 1
+			ctx.beginPath();
+			ctx.moveTo(105, 85);
+			ctx.lineTo(152, 102);
+			ctx.stroke();
+			ctx.closePath();
 
 // LINEA (VISTA SUPERIOR) (1)
 ctx.beginPath();
-ctx.moveTo(195, 47);
-ctx.lineTo(165, 62);
-ctx.lineTo(160, 103);
-ctx.lineTo(130, 122);
-ctx.lineTo(130, 137);
+ctx.lineTo(170, 90);
+ctx.lineTo(120, 120);
+ctx.lineTo(120, 135);
 ctx.stroke();
 ctx.closePath();
-				// LINEA PARTE DERECHA 
-				ctx.beginPath();
-				// ctx.moveTo(185, );
-				// ctx.lineTo(X, Y);
-				ctx.stroke();
-				ctx.closePath();
+				
+// LINEA (VISTA SUPERIOR) (2)
+ctx.beginPath();
+ctx.moveTo(148, 30);
+ctx.lineTo(122, 45);
+ctx.lineTo(122, 75);
+ctx.lineTo(65, 105);
+ctx.lineTo(65, 120);
+ctx.lineTo(105, 98);
+ctx.lineTo(105, 85);
+ctx.stroke();
+ctx.closePath();
+
+		// LIENA MEDIO INFERIOR Conector
+		ctx.beginPath();
+		ctx.moveTo(105, 98);
+		ctx.lineTo(138, 110);
+		ctx.stroke();
+		ctx.closePath();
+
+// LINEA BASE ATRAS (CUBO)
+ctx.beginPath();
+ctx.moveTo(120, 20);
+ctx.lineTo(120, 50);
+ctx.lineTo(95, 65);
+ctx.stroke();
+ctx.closePath();
+	
+		
+			// LINEA BASE IZQUIERDA (2)
+			ctx.beginPath();
+			ctx.moveTo(120, 35);
+			ctx.lineTo(220, 70);
+			ctx.lineTo(195, 85);
+			ctx.lineTo(95, 50);
+			ctx.lineTo(120, 35);
+			ctx.stroke();
+			ctx.closePath();
+
+
+			// Cubo linea terminal
+			ctx.beginPath();
+			ctx.moveTo(122, 75);
+			ctx.lineTo(148, 60);
+			ctx.lineTo(148, 30);
+			ctx.stroke();
+			ctx.closePath();
