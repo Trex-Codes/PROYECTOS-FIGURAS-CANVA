3b2a0d7dd923d706ea7a738e5a8f891998758e4c
Trex_Codes <juan.13JSG@gmail.com>
2020-06-19 22:14:10 -0500

    12.0 Figure 2 finish - missing the views

--- Figura_02.js
+++ Figura_02.js
@@ -1,12 +1,10 @@
 // FIGURA
-// function ViewFigure2(){
+function ViewFigure2(){
 
 	var canvas = document.getElementById('CanvSect2');
 	var ctx = canvas.getContext('2d');
   
   	ctx.clearRect(0, 0, canvas.width, canvas.height);
-// }
-
 
 // linea cental - (BASE CUBO)
 ctx.beginPath();
@@ -22,28 +20,7 @@ ctx.moveTo(35, 95);
 ctx.lineTo(65, 105);
 ctx.stroke();
 ctx.closePath();	
-
-// linea atras derecha - (BASE CUBO)
-ctx.beginPath();
-ctx.moveTo(147, 30); // Linea atras Union Cubo (SUPERIOR)
-ctx.lineTo(120, 20); 
-ctx.stroke();
-ctx.closePath();
 	
-			// Linea conector cuadro derecho
-			ctx.beginPath();
-			ctx.moveTo(220, 55);
-			ctx.lineTo(195, 70);
-			ctx.stroke();
-			ctx.closePath();
-
-// cuadro izquierdo completo
-ctx.beginPath();
-ctx.moveTo(120, 20);
-ctx.lineTo(95, 35); // Conector base Arriba - Base Abajo 
-ctx.stroke();
-ctx.closePath();
-
 	// LINEA INFERIOR (1)
 	ctx.beginPath();
 	ctx.moveTo(120, 120);
@@ -58,7 +35,7 @@ ctx.closePath();
 	ctx.lineTo(150, 145);
 	ctx.moveTo(150, 145); // Linea derecha (BASE CUBO)
 	ctx.lineTo(220, 100);
-	ctx.lineTo(220, 55); // linea 90° derecha 
+	ctx.lineTo(220, 70); // linea 90° derecha 
 	ctx.stroke();
 	ctx.closePath();
 
@@ -66,9 +43,14 @@ ctx.closePath();
 
 // Linea medio (VISTA LATERAL)
 ctx.beginPath();
-ctx.moveTo(195, 115);
-ctx.lineTo(195, 70);
-ctx.lineTo(95, 35);
+ctx.moveTo(195, 100);
+ctx.lineTo(195, 85); 
+ctx.stroke();
+ctx.closePath();
+
+// Linea completa izquierda  //////////////////
+ctx.beginPath();
+ctx.lineTo(95, 25);
 ctx.lineTo(95, 65);
 ctx.lineTo(35, 95);
 ctx.lineTo(35, 110);
@@ -77,15 +59,6 @@ ctx.lineTo(65, 120);
 ctx.stroke();
 ctx.closePath();
 				
-				// LINEA CONECTOR 90° CUADRO BAJO
-/*				ctx.beginPath();
-				ctx.moveTo(95, 65);
-				ctx.lineTo(95, 80);
-				ctx.stroke();
-				ctx.closePath();
-*/
-
-
 // LINEA MEDIO BASE INFERIOR (1)
 ctx.beginPath();
 ctx.moveTo(195, 100);
@@ -93,17 +66,16 @@ ctx.lineTo(95, 65);
 ctx.stroke();
 ctx.closePath();
 
-
-			// Linea 1
+			// Linea inferior 
 			ctx.beginPath();
 			ctx.moveTo(105, 85);
-			ctx.lineTo(152, 102);
+			ctx.lineTo(150, 101);
 			ctx.stroke();
 			ctx.closePath();
 
 // LINEA (VISTA SUPERIOR) (1)
 ctx.beginPath();
-ctx.lineTo(170, 90);
+ctx.lineTo(152, 101);
 ctx.lineTo(120, 120);
 ctx.lineTo(120, 135);
 ctx.stroke();
@@ -111,9 +83,7 @@ ctx.closePath();
 				
 // LINEA (VISTA SUPERIOR) (2)
 ctx.beginPath();
-ctx.moveTo(148, 30);
-ctx.lineTo(122, 45);
-ctx.lineTo(122, 75);
+ctx.lineTo(105, 85);
 ctx.lineTo(65, 105);
 ctx.lineTo(65, 120);
 ctx.lineTo(105, 98);
@@ -121,37 +91,86 @@ ctx.lineTo(105, 85);
 ctx.stroke();
 ctx.closePath();
 
-		// LIENA MEDIO INFERIOR Conector
+		// LINEA MEDIO INFERIOR Conector
 		ctx.beginPath();
 		ctx.moveTo(105, 98);
 		ctx.lineTo(138, 110);
 		ctx.stroke();
 		ctx.closePath();
 
-// LINEA BASE ATRAS (CUBO)
-ctx.beginPath();
-ctx.moveTo(120, 20);
-ctx.lineTo(120, 50);
-ctx.lineTo(95, 65);
-ctx.stroke();
-ctx.closePath();
-	
 		
 			// LINEA BASE IZQUIERDA (2)
 			ctx.beginPath();
 			ctx.moveTo(120, 35);
 			ctx.lineTo(220, 70);
 			ctx.lineTo(195, 85);
-			ctx.lineTo(95, 50);
-			ctx.lineTo(120, 35);
+			ctx.lineTo(125, 60);
+			// ctx.lineTo(120, 35);
 			ctx.stroke();
 			ctx.closePath();
 
+					// Liena atras izquierda
+					ctx.beginPath();
+					ctx.moveTo(120, 35);
+					ctx.lineTo(95, 50);
+					ctx.stroke();
+					ctx.closePath();
 
-			// Cubo linea terminal
+// Cubo linea terminal 
+ctx.beginPath();
+ctx.moveTo(125, 60);
+ctx.lineTo(149, 45);
+ctx.lineTo(120, 10);
+ctx.lineTo(95, 25);
+ctx.lineTo(125, 60);
+ctx.stroke();
+ctx.closePath();
+					
+	// linea mitad atras 
+	ctx.beginPath();
+	ctx.moveTo(120, 10);
+	ctx.lineTo(120, 35);
+	ctx.stroke();
+	ctx.closePath();
+
+			// LINEA CONECTOR 2 TRIANGULOS -----
 			ctx.beginPath();
-			ctx.moveTo(122, 75);
-			ctx.lineTo(148, 60);
-			ctx.lineTo(148, 30);
+			ctx.moveTo(105, 65);
+			ctx.lineTo(75, 55);
+			ctx.lineTo(57, 73);
 			ctx.stroke();
 			ctx.closePath();
+
+			// recta parte izquierda cuadrado 
+			ctx.beginPath();
+			ctx.moveTo(75, 55);
+			ctx.lineTo(75, 75);
+			ctx.lineTo(105, 85);
+			ctx.stroke();
+			ctx.closePath();
+
+		// recta 90 (2 DERECHA)
+		ctx.beginPath();
+		ctx.moveTo(105, 85);
+		ctx.lineTo(105, 65);
+		ctx.lineTo(87, 83);
+		ctx.stroke();
+		ctx.closePath();
+
+// Linea parte 1
+ctx.beginPath();			
+ctx.lineTo(87, 94);
+ctx.lineTo(57, 85);
+ctx.stroke();
+ctx.closePath();
+
+// cuadro TRIANGULOS
+ctx.beginPath();
+ctx.moveTo(57, 85);
+ctx.lineTo(57, 73);
+ctx.lineTo(87, 83);
+ctx.lineTo(87, 95);
+ctx.stroke();
+ctx.closePath();
+
+}
