0abef111411fd1936ebc45ab9698d9df5fb0a7dd
Trex_Codes <juan.13JSG@gmail.com>
2020-06-26 20:27:29 -0500

    ---------- 21.0 FIGURE 5 FINISH ----------

--- Figura_05.js
+++ Figura_05.js
@@ -63,3 +63,137 @@ ctx.lineTo(220, 60);
 ctx.stroke();
 ctx.closePath();
 }
+
+// VISTA SUPERIOR
+function ViewVist_Sup05(){
+
+	var canvas = document.getElementById('CanvSect5');
+  	var ctx = canvas.getContext('2d');
+  
+ 	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	// Triangulo amarillo 
+	ctx.beginPath();
+	ctx.fillStyle = "rgba(255, 229, 0, 0.7)";
+	ctx.moveTo(25, 135);
+	ctx.lineTo(25, 15);
+	ctx.lineTo(275, 15);	
+	ctx.lineTo(25, 135);
+	ctx.stroke();
+	ctx.fill();
+	ctx.closePath();
+
+	// Triangulo rojo
+	ctx.beginPath();
+	ctx.fillStyle = "rgba(249, 16, 16, 0.7)";
+	ctx.moveTo(275, 135);
+	ctx.lineTo(275, 15);
+	ctx.lineTo(25, 135);
+	ctx.lineTo(275, 135);
+	ctx.stroke();
+	ctx.fill();
+	ctx.closePath();
+}
+
+// VISTA FRONTAL
+function ViewVist_Front05(){
+
+	var canvas = document.getElementById('CanvSect5');
+  	var ctx = canvas.getContext('2d');
+  
+ 	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	// ---------- SECCION SUPERIOR ---------- 
+
+	// cuadro largo 
+	ctx.beginPath();
+	ctx.fillStyle = "rgba(232, 235, 214, 0.7)";
+	ctx.fillRect(25, 15, 250, 30);
+	ctx.strokeRect(25, 15, 250, 30);
+	ctx.stroke();
+	ctx.closePath();
+
+
+	// ---------- SECCION INFERIOR ---------- 
+
+	// triangulo medio
+	ctx.beginPath();
+	ctx.fillStyle = "rgba(249, 16, 16, 0.7)";
+	ctx.moveTo(150, 105);
+	ctx.lineTo(25, 45);
+	ctx.lineTo(275, 45);
+	ctx.lineTo(150, 135);
+	ctx.stroke();
+	ctx.fill();
+	ctx.closePath();
+
+	// trapecio izquierdo 
+	ctx.beginPath();
+	ctx.fillStyle = "rgba(255, 229, 0, 0.7)";
+	ctx.moveTo(150, 135);
+	ctx.lineTo(150, 105);
+	ctx.lineTo(25, 45);
+	ctx.lineTo(25, 135);
+	ctx.lineTo(150, 135);
+	ctx.stroke();
+	ctx.fill();
+	ctx.closePath();	
+
+	// trapecio derecho
+	ctx.beginPath();
+	ctx.fillStyle = "lightblue";
+	ctx.moveTo(150, 135);
+	ctx.lineTo(150, 105);
+	ctx.lineTo(275, 45);
+	ctx.lineTo(275, 135);
+	ctx.lineTo(150, 135);
+	ctx.stroke();
+	ctx.fill();
+	ctx.closePath();
+}
+
+// VISTA LATERAL
+function ViewVist_Lat05(){
+
+	var canvas = document.getElementById('CanvSect5');
+  	var ctx = canvas.getContext('2d');
+  
+ 	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	
+// ---------- SECCION SUPERIOR ---------- 
+
+	// cuadrado superior medio 
+	ctx.beginPath();
+	ctx.fillStyle = "rgba(232, 235, 214, 0.7)";
+	ctx.fillRect(25, 15, 250, 30);
+	ctx.strokeRect(25, 15, 250, 30);
+	ctx.stroke();
+	ctx.closePath();
+
+// ---------- SECCION INFERIOR ---------- 
+
+	// trapecio medio 
+	ctx.beginPath();
+	ctx.fillStyle = "rgba(249, 16, 16, 0.7)";
+	ctx.moveTo(25, 45);
+	ctx.lineTo(275, 45);
+	ctx.lineTo(25, 105);
+	ctx.lineTo(25, 45);
+	ctx.stroke();
+	ctx.fill();
+	ctx.closePath();
+
+	// trapecio inferior
+	ctx.beginPath();
+	ctx.fillStyle = "lightblue";
+	ctx.moveTo(25, 105);
+	ctx.lineTo(25, 135);
+	ctx.lineTo(275, 135);
+	ctx.lineTo(275, 45);
+	ctx.lineTo(25, 105);
+	ctx.stroke();
+	ctx.fill();
+	ctx.closePath();
+}
+
+
