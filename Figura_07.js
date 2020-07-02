c2146bca393bd12155c38d29187984e775767ede
Trex_Codes <juan.13JSG@gmail.com>
2020-07-01 23:02:32 -0500

    26.0 Structure figure #7 finish and missing the views

--- Figura_07.js
+++ Figura_07.js
@@ -5,12 +5,10 @@
 	var ctx = canvas.getContext('2d');
   
   	ctx.clearRect(0, 0, canvas.width, canvas.height);
-// }
-
 
 // linea cental - (BASE CUBO)
 ctx.beginPath();
-ctx.moveTo(150, 100);
+ctx.moveTo(150, 130);
 ctx.lineTo(150, 145);
 ctx.stroke();
 ctx.fill();
@@ -20,8 +18,8 @@ ctx.closePath();
 ctx.beginPath();
 ctx.moveTo(150, 145); // Linea derecha (BASE CUBO)
 ctx.lineTo(220, 100);
-ctx.lineTo(220, 55); // linea 90° derecha 
-ctx.lineTo(150, 100); // conectores linea central superior
+ctx.lineTo(220, 40); // linea 90° derecha 
+ctx.lineTo(200, 52); // conectores linea central superior
 ctx.stroke();
 ctx.closePath();
 
@@ -29,33 +27,109 @@ ctx.closePath();
 ctx.beginPath();
 ctx.moveTo(150, 145); // Liena izquierda (BASE CUBO)
 ctx.lineTo(50, 100);
-ctx.lineTo(50, 60); // linea 90° izquierda
-ctx.lineTo(150, 100); // conectores linea central superior
+ctx.lineTo(50, 40); // linea 90° izquierda
+ctx.lineTo(70, 50);
 ctx.stroke();
 ctx.closePath();	
 
 // linea atras derecha - (BASE CUBO)
 ctx.beginPath();
-ctx.moveTo(220, 55); // Linea atras Union Cubo (SUPERIOR)
-ctx.lineTo(120, 20); 
+ctx.moveTo(220, 40); // Linea atras Union Cubo (SUPERIOR)
+ctx.lineTo(120, 10); 
 ctx.stroke();
 ctx.closePath();
 
 // cuadro izquierdo completo
 ctx.beginPath();
-ctx.lineTo(120, 20);
-ctx.lineTo(50, 60); // Conector base Arriba - Base Abajo 
+ctx.lineTo(120, 10);
+ctx.lineTo(50, 40); // Conector base Arriba - Base Abajo 
 ctx.stroke();
 ctx.closePath();
-
-// linea inferior cara #2 - #3
-ctx.beginPath();
-ctx.moveTo(50, 85); // Linea barra Base 2.0 Izquierda 
-ctx.lineTo(150, 130);
-ctx.lineTo(220, 85); // Linea barra base 2.0 (Vista  frontal- lateral)
-ctx.stroke();
-ctx.closePath();
-
-	
 
 // --------------------------------------------
+
+
+// linea lateral 180 (abajo - arriba) (1)
+ctx.beginPath();
+ctx.moveTo(150, 130);
+ctx.lineTo(179, 112);
+
+ctx.stroke();
+ctx.closePath();
+
+// --------------------------------------------
+
+// linea lateral 90 superior (1)
+ctx.beginPath();
+ctx.moveTo(200, 75);
+ctx.lineTo(200, 52);
+ctx.lineTo(125, 27);
+ctx.stroke();
+ctx.closePath();
+
+// linea lateral 90 superior (2)
+ctx.beginPath();
+ctx.moveTo(179, 112);
+ctx.lineTo(179, 90);
+ctx.lineTo(100, 60);
+ctx.lineTo(100, 80);
+ctx.stroke();
+ctx.closePath();
+
+// cuadro 3 
+ctx.beginPath();
+ctx.moveTo(100, 60);
+ctx.lineTo(125, 48);
+ctx.lineTo(200, 75);
+ctx.lineTo(177, 91);
+ctx.stroke();
+ctx.closePath();
+
+// --------------------------------------------
+
+// linea frontal 90  (1)
+ctx.beginPath();
+ctx.moveTo(70, 95);
+ctx.lineTo(70, 50);
+ctx.lineTo(125, 26);
+ctx.lineTo(125, 67);
+ctx.lineTo(70, 95);
+ctx.stroke();
+ctx.closePath();
+
+// liena 180 frontal (abajo - izquierda) (1)
+ctx.beginPath();
+ctx.moveTo(70, 95);
+ctx.lineTo(150, 130);
+ctx.stroke();
+ctx.closePath();
+
+// medio cuadro 1-2 
+ctx.beginPath();
+ctx.moveTo(179, 112);
+ctx.lineTo(100, 80);
+ctx.stroke();
+ctx.closePath();
+
+// --------------------------------------------
+ // cuadro pequeño 
+ ctx.beginPath();
+ ctx.moveTo(85, 55);
+ ctx.lineTo(65, 45);
+ ctx.lineTo(65, 33);
+ ctx.lineTo(85, 43);
+ ctx.lineTo(85, 55);
+ ctx.lineTo(108, 45);
+ ctx.lineTo(108, 33);
+ ctx.lineTo(87, 24);
+ ctx.lineTo(87, 37);
+ ctx.lineTo(108, 45);
+ ctx.stroke();
+ ctx.closePath();
+
+ // corte atras cuadro pequeño 
+ ctx.beginPath();
+ ctx.moveTo(87, 37);
+ ctx.lineTo(65, 45);
+ ctx.stroke();
+ctx.closePath();
