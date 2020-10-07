8a59108d683d11d26d46f0ad8da4d50b8554b39c
juan sebastian <juan.13JSG@gmail.com>
2020-10-07 12:09:49 -0500

    ---------- 43.0 FIGURE 12 FINISH ----------

--- Figura_12.js
+++ Figura_12.js
@@ -189,68 +189,12 @@ ctx.closePath();
 }
 
 // VISTA LATERAL
-// function ViewVist_Lat12(){
+function ViewVist_Lat12(){
 
 	var canvas = document.getElementById('CanvSect12');
   	var ctx = canvas.getContext('2d');
   
  	ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-	// PLANO PRINCIPAL
-	ctx.beginPath();
-	ctx.fillStyle = 'rgba(240, 235, 234, 0.9)';
-	ctx.strokeRect(25, 15, 250, 120);
-	ctx.closePath();
-
-	// Liena 90 izquierda 1
-	ctx.beginPath();
-	ctx.moveTo(75, 15);
-	ctx.lineTo(75, 135);
-	ctx.stroke();
-	ctx.closePath();
-
-	// liena 90 izquierda 2 
-	ctx.beginPath();
-	ctx.moveTo(125, 15);
-	ctx.lineTo(125, 135);
-	ctx.stroke();
-	ctx.closePath();
-
-	// Linea izquierda 3
-	ctx.beginPath();
-	ctx.moveTo(175, 15);
-	ctx.lineTo(175, 135);
-	ctx.stroke();
-	ctx.closePath();
-
-	// linea izquierda 4
-	ctx.beginPath();
-	ctx.moveTo(225, 15);
-	ctx.lineTo(225, 135);
-	ctx.stroke();
-	ctx.closePath();
-// ----------------------------
-
-	// linea inferior 1
-	ctx.beginPath();
-	ctx.moveTo(25, 45);
-	ctx.lineTo(275, 45);
-	ctx.stroke();
-	ctx.closePath();
-
-	// linea inferior 2 
-	ctx.beginPath();
-	ctx.moveTo(25, 75);
-	ctx.lineTo(275, 75);
-	ctx.stroke();
-	ctx.closePath();
-
-	// linea inferior 3 
-	ctx.beginPath();
-	ctx.moveTo(25, 105);
-	ctx.lineTo(275, 105);
-	ctx.stroke();
-	ctx.closePath();
 
 	// ---------- FIGURA TOTAL ----------
 
@@ -268,4 +212,25 @@ ctx.stroke();
 ctx.fill();
 ctx.closePath();
 
-// }
+	// ---------- SECCION LATERAL IZQUIERDA ----------
+
+// cuadro figura 
+ctx.beginPath();
+ctx.fillStyle = "rgba(255, 255, 255, 0.7)";
+ctx.strokeRect(25, 15, 50, 90);
+ctx.fillRect(25, 15, 50, 90);
+ctx.stroke();
+ctx.fill();
+ctx.closePath();
+
+	// ---------- SECCION MEDIO ----------
+
+// cuadro medio
+ctx.beginPath();
+ctx.fillStyle = "pink";
+ctx.strokeRect(75, 15, 150, 90);
+ctx.fillRect(75, 15, 150, 90);
+ctx.stroke();
+ctx.fill();
+ctx.closePath();
+}
